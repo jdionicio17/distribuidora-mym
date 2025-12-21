d68826f7c2a8e5908bbf284dfdbe9b252e026e47
Jose Dionicio <tu_correo_de_github@gmail.com>
2025-12-21 13:38:28 -0700

    Actualizar configuración DB Railway y scripts

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,18 @@
-// backend/src/config/db.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'distribuidoramym',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  // Si da error SSL, activamos esto:
+  // ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
